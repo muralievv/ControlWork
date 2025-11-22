6733091e8264afceef209e60d42624a5b945c458
muralievv <nmuraliev@gmail.com>
2025-11-22 23:31:22 +0600

    a

--- oop.py
+++ oop.py
@@ -3,44 +3,45 @@ class Person:
         self._age = age
     def get_age(self):
         if self._age >= 0:
-         return (self._age)
+         return self._age
         else:
             print(f"Ошибка, возраст не может быть меньше 0")
 
 p = Person()
 p.set_age(25)
+
 print(p.get_age())
 
 class Animal:
     def __init__(self, name):
         self.name = name
     def speak(self):
-        return (f"I am an animal")
+        return f"I am an animal"
 class Dog(Animal):
     def speak(self):
-        return (f"Woof")
+        return f"Woof"
 class Cat(Animal):
     def speak(self):
-        return (f"Meow")
+        return f"Meow"
 dog = Dog("buddy")
 cat = Cat("Kitty")
 print(dog.name, dog.speak())
 print(cat.name, cat.speak())
 class Vehicle:
     def move(self):
-        return (f"Vehicle is moving")
+        return f"Vehicle is moving"
 class Car(Vehicle):
     def move(self):
-        return (f"Car is driving")
-class Bycicle(Vehicle):
+        return f"Car is driving"
+class Bicycle(Vehicle):
     def move(self):
-        return (f"Bycicle is pedaling")
-def move(Vehicle):
-    print (Vehicle.move())
+        return f"Bicycle is pedaling"
+def move(vehicle):
+    print (vehicle.move())
 car = Car()
-bike = Bycicle()
-move(car)
-move(bike)
+bike = Bicycle()
+print(move(car))
+print(move(bike))
 
 from abc import ABC, abstractmethod
 class Shape(ABC):
@@ -52,12 +53,12 @@ class Rectangle(Shape):
          self.width = width
          self.height = height
      def area(self):
-         return(self.height * self.width)
+         return self.height * self.width
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
     def area(self):
-        return(self.radius ** 2 * 3.14)
+        return self.radius ** 2 * 3.14
 rect = Rectangle(10,5)
 circle = Circle(7)
 print(rect.area())
